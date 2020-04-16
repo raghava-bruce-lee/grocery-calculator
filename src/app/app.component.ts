948e9fa71b199ea262ff0f9d4ba967f35f93041b
raghavendra <raghavendra1319@gmail.com>
2020-04-16 20:25:58 +0530

    alpha testing

---
+++ src/app/app.component.ts
@@ -0,0 +1,112 @@
+import { Component } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'grocery-tracker';
+
+  selected_item_value : string;
+  quantity_value : number;
+  metrix_value : string;
+  auto_metrics_value: any;
+  person_count_value : number;
+  days : any;
+  sortedGroceryItems: any;
+
+  url = 'https://script.google.com/macros/s/AKfycbzs5fSlNwSws_KvcGglhscY50uGZRywD_Tajmx1RzMzEGR1xq6Q/exec';
+  groceryData : any;
+
+  constructor (private http: HttpClient) {
+    this.http.get(this.url).toPromise().then( data => {
+      this.groceryData = data as any;
+
+      /* sorting function for json data */
+
+      function GetSortOrder(prop) {
+        return function(a, b) {
+            if (a[prop] > b[prop]) {
+                return 1;
+            } else if (a[prop] < b[prop]) {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    this.sortedGroceryItems = this.groceryData.items.sort(GetSortOrder("groceryitem"));
+    /* ending for sorting function */
+
+    } );
+   }
+
+   getMetrics(){
+     if( this.selected_item_value == null ){
+       alert("please select the grocery item first");
+     }
+    for( var i=0; i< this.groceryData.items.length; i++){
+      if ((this.groceryData.items[i].groceryitem) == this.selected_item_value){
+        this.auto_metrics_value = this.groceryData.items[i].metrics;
+      }
+    }
+  }
+
+  selectedItemHandler(event: any){
+    this.selected_item_value = event.target.value;
+  }
+
+  selectedMetricsHandler(event: any){
+    this.metrix_value = event.target.value;
+  }
+
+  gettingQuantityValue(event: any){
+    this.quantity_value = event.target.value;
+  }
+
+  gettingPersonCount(event: any){
+    this.person_count_value = event.target.value;
+  }
+
+
+
+  calculate(){
+
+    if (this.auto_metrics_value == null){
+      alert ('Please click on "Get Metrics" - button and enter your qunatity according to that displayed metrics');
+    }
+    else{
+
+    for (var i=0; i<(this.sortedGroceryItems.length); i++){
+
+      if ((this.sortedGroceryItems[i].groceryitem) == this.selected_item_value){
+
+          var qpd = this.sortedGroceryItems[i].peruse;
+
+          var res = (this.quantity_value/this.person_count_value);
+
+          if (res > qpd){
+            this.days = Math.floor((res/qpd)) + "  days";
+          }
+          else if (res == qpd){
+            this.days = "only " + 1 + " day";
+          }
+          else if (res < qpd){
+            this.days = "less than a day";
+          }
+      }
+    }
+  }
+
+  }
+
+   reload(){
+     /*window.location.reload();*/
+   }
+}
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatTabsModule,
+    MatDialogModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
